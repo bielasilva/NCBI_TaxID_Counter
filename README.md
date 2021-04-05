328dd7a258d06d599ba39ce1bcf9540205ec4e2c
bielasilva <gabrielamorimsilva@gmail.com>
2021-04-05 20:43:18 -0300

    Updated README.md; Added to save Venn diagrams as species; Better documented the code

--- README.md
+++ README.md
@@ -1,28 +1,22 @@
 # NCBI TaxID Counter
-
 This project is part of a collection of codes developed for my Undergraduate Thesis.
 
 ## Dependencies
-
 * Biopython - Bio.Entrez package
+* Matplotlib - venn2, venn2_circle, pyplot
 * Modules - csv, os and sys
 * python 3.6+
 
 ## Features
-
 * It takes the result files from Metagenome Taxonomy Classification Tools and sorts the TaxIDs between the taxonomy ranks (Superkingdom, Kingdom, Phylum, Class, Order, Family, Genus and Species) and the groups used by the programs (Bacteria, Archaea, Viruses, Fungi, Protozoa)
 
 ## Usage
-
 ### Arguments
-
 It can handle as many files as need, each being processed individually.
-
 #### Example:
 ```bash
-./taxid_counter.py krona_file metacache_file centrifuge_file [...]
+./taxid_counter.py (-compare) krona_file metacache_file centrifuge_file [...] 
 ```
 ### Output
-
 - The output is a CSV file where the rows represent same rank and the columns same group. The name is the original file name + _sorted.csv" 
-- It also outputs Venn diagrams in SVG format for Bacteria, Archaea, Viruses, Fungi and Protozoa.
+- It also outputs Venn diagrams in SVG format for Bacteria, Archaea, Viruses, Fungi and Protozoa with total TaxIds and only species.
--- taxid_counter.py
+++ taxid_counter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# Requirements
 from Bio import Entrez
 from matplotlib_venn import venn2, venn2_circles
 from matplotlib import pyplot as plt
@@ -8,27 +9,38 @@ import csv, os, sys
 def main():
     """Function to  run the program."""
     print("\n## Beginning program ##")
+    # In case compare function is not selected, run this
     if sys.argv[1] != "-compare":
         print("Normal mode selected")
+        # Gets the files from the arguments
         for file in sys.argv[1:]:
             core_out = core(file)
             print(f"Done with {core_out[0]} \n")
+    # In case compare function is selected, run this
     if sys.argv[1] == "-compare":
         print("Compare mode selected")
         compare()
+        print("Finished compare module")
 
-def core(file):
+def core(file): # input is a taxonomic profiling output file 
     """Program core, runs the standard operations (getting the TaxIDs and counting them"""
+    # Gets the path and name of each file
     metag_result_file = str(os.path.abspath(file))
     file_name = metag_result_file.split("/")[-1]
+    # Gets the TaxIds from the profiling output files
     get_taxids_out = get_taxids(metag_result_file, file_name)
+    # Gets the data from NCBI of each TaxId
     get_tax_data_out = get_tax_data(get_taxids_out)
+    # Organizes and classifies the data
     sort_tax_data_out = sort_tax_data(get_tax_data_out, file_name)
+    # Saves the output of each file classification as a CSV file
     save_file(f"{file_name}_sorted.csv", sort_tax_data_out, ["superkingdom", "kingdom", "phylum", "class", "order", "family", "genus", "species"])
     return file_name, sort_tax_data_out
 
-def compare():
+def compare(): # no input needed
+    """Compares the TaxIds"""
     complete_data = {}
+    # Gets the files from the arguments
     for file in sys.argv[2:]:
         core_out = core(file)
         if "centrifuge" in core_out[0]:
@@ -41,25 +53,27 @@ def compare():
             complete_data["metaphlan"] = core_out[1]
         print(f"Done with {core_out[0]} \n")
     print("Entering compare module")
-    # with open("complete_data.txt", "w+") as out:
-    #     print(complete_data, file=out)
+    # Creating the dictionaries to store the results
     shared_taxids = {"bacteria": {}, "archaea": {}, "viruses": {}, "fungi": {}, "protozoa": {}, "rest": "Not applicable"}
     centrifuge_taxids = {"bacteria": set([]), "archaea": set([]), "viruses": set([]), "fungi": set([]), "protozoa": set([])}
     kraken_taxids = {"bacteria": set([]), "archaea": set([]), "viruses": set([]), "fungi": set([]), "protozoa": set([])}
+    # Setting up the parameters for classification and comparasion 
     ranks = ["phylum", "class", "order", "family", "genus", "species"]
     groups = ["bacteria", "archaea", "viruses", "fungi", "protozoa"]
-
+    
     for group in groups:
         for rank in ranks:
+            # Gets the TaxIds in common between each group and taxonomic rank
             shared_taxids[group][rank] = complete_data["centrifuge"][group][rank].intersection(complete_data["kraken"][group][rank])
+            # Creates a list for each group with all TaxIds from every rank
             centrifuge_taxids[group].update(complete_data["centrifuge"][group][rank])
             kraken_taxids[group].update(complete_data["kraken"][group][rank])
-
+    # Prints on the screen the results for each group and taxonomic rank
     for group in groups:
         print(f"\n{group}")
         for key, value in shared_taxids[group].items():
             print(key, "->", len(value))
-    
+    # Creates Venn diagrams for each group with all ranks
     for group in groups:
         venn2([centrifuge_taxids[group], kraken_taxids[group]],
             set_labels = ('Centrifuge', 'Kraken2'),
@@ -68,31 +82,18 @@ def compare():
         plt.title(group)
         plt.savefig(f"{group}.svg", transparent=True)
         plt.clf()
-
+    # Creates Venn diagrams for each group with only species
+    for group in groups:
+        venn2([complete_data["centrifuge"][group]["species"], complete_data["kraken"][group]["species"]],
+                set_labels=('Centrifuge', 'Kraken2'),
+                set_colors=('red', 'blue'), alpha=0.4)
+        venn2_circles([complete_data["centrifuge"][group]["species"],
+                        complete_data["kraken"][group]["species"]], linewidth=0.5)
+        plt.title(group)
+        plt.savefig(f"{group}_species.svg", transparent=True)
+        plt.clf()
+    # Saves the result of the comparasions to a CSV file
     save_file("shared_kraken2_centrifuge.csv", shared_taxids, ranks)
-    
-    # shared_bacteria_names = {}
-    # shared_fungi_names = {}
-    # shared_viruses_names = {}
-    # shared_archaea_names = {}
-    # for key, value in shared_bacteria_taxid.items():
-    #     print(f"Getting data from Bacteria {key}")
-    #     get_tax_data_out = get_tax_data(list(value))
-    #     shared_bacteria_names[f'{key}'] = [item["ScientificName"] for item in get_tax_data_out]
-    # for key, value in shared_archaea_taxid.items():
-    #     print(f"Getting data from archaea {key}")
-    #     get_tax_data_out = get_tax_data(list(value))
-    #     shared_archaea_names[f'{key}'] = [item["ScientificName"] for item in get_tax_data_out]
-    # for key, value in shared_fungi_taxid.items():
-    #     print(f"Getting data from fungi {key}")
-    #     get_tax_data_out = get_tax_data(list(value))
-    #     shared_fungi_names[f'{key}'] = [item["ScientificName"] for item in get_tax_data_out]
-    # for key, value in shared_viruses_taxid.items():
-    #     print(f"Getting data from viruses {key}")
-    #     get_tax_data_out = get_tax_data(list(value))
-    #     shared_viruses_names[f'{key}'] = [item["ScientificName"] for item in get_tax_data_out]
-        
-    # print(shared_bacteria_names,"\n", shared_archaea_names,"\n", shared_fungi_names,"\n", shared_viruses_names)
 
 # This function gets the TaxIDs from the report file
 def get_taxids(in_file, origin):
@@ -102,6 +103,7 @@ def get_taxids(in_file, origin):
     with open(in_file) as csv_read:
         mg_result = csv.reader(csv_read, delimiter='\t')
         metaphlan = False
+        # Checks to see which program output file it is
         if any(st in in_file for st in ["centrifuge", "krk"]): #centrifuge, kraken2 and metaplhan3
             index = 2
         elif "metacache" in in_file: #metacache
@@ -112,6 +114,7 @@ def get_taxids(in_file, origin):
         else:
             print("File format not recognized")
             exit()
+        # Reads the files
         for row in mg_result:
             if not row[0].startswith("#"):
                 if metaphlan:
@@ -142,7 +145,7 @@ def get_tax_data(list):
 def sort_tax_data(data_list, origin):
     """Takes the data from the TaxIDs and sorts it to each taxonomy level and category (Bacteria, archaea, viruses, fungi and protozoa) and returns it."""
     print("Sorting Tax data")
-
+    # Creating the dictionaries to store the results
     bacteria = {"superkingdom": set([]), "kingdom": set([]), "phylum": set([]), "class": set([]), "order": set([]), "family": set([]), "genus": set([]), "species": set([])}
     archaea = {"superkingdom": set([]), "kingdom": set([]), "phylum": set([]), "class": set([]), "order": set([]), "family": set([]), "genus": set([]), "species": set([])}
     viruses = {"superkingdom": set([]), "kingdom": set([]), "phylum": set([]), "class": set([]), "order": set([]), "family": set([]), "genus": set([]), "species": set([])}
@@ -153,7 +156,7 @@ def sort_tax_data(data_list, origin):
     
     try:
         for entry in data_list:
-            trying = entry
+            # Checks if it is on of the special TaxIds
             if entry["TaxId"] in ("1", "131567", "10239", "2157"):
                 if entry["TaxId"] in ("1", "131567"):
                     rest.add(entry["TaxId"])
@@ -161,22 +164,27 @@ def sort_tax_data(data_list, origin):
                     viruses["superkingdom"].add(entry["TaxId"])
                 elif entry["TaxId"] == "2157":
                     archaea["superkingdom"].add(entry["TaxId"])
+            # Checks if it is Bacteria TaxId
             elif "2" in (entry["TaxId"], (next((item["TaxId"] for item in entry["LineageEx"] if item["TaxId"] == "2"), None))):
                 for rank in ranks:
                     if (next((item["TaxId"] for item in entry["LineageEx"] if item["Rank"] == rank), None)) != None:
                         bacteria[rank].add((next((item["TaxId"] for item in entry["LineageEx"] if item["Rank"] == rank), None)))
+            # Checks if it is Archaea TaxId
             elif "2157" in (entry["TaxId"], (next((item["TaxId"] for item in entry["LineageEx"] if item["TaxId"] == "2157"), None))):
                 for rank in ranks:
                     if (next((item["TaxId"] for item in entry["LineageEx"] if item["Rank"] == rank), None)) != None:
                         archaea[rank].add((next((item["TaxId"] for item in entry["LineageEx"] if item["Rank"] == rank), None)))
+            # Checks if it is Virus TaxId
             elif "10239" in (entry["TaxId"], (next((item["TaxId"] for item in entry["LineageEx"] if item["TaxId"] == "10239"), None))):
                 for rank in ranks:
                     if (next((item["TaxId"] for item in entry["LineageEx"] if item["Rank"] == rank), None)) != None:
                         viruses[rank].add((next((item["TaxId"] for item in entry["LineageEx"] if item["Rank"] == rank), None)))
+            # Checks if it is Fungi TaxId
             elif "4751" in (entry["TaxId"], (next((item["TaxId"] for item in entry["LineageEx"] if item["TaxId"] == "4751"), None))):
                 for rank in ranks:
                     if (next((item["TaxId"] for item in entry["LineageEx"] if item["Rank"] == rank), None)) != None:
                         fungi[rank].add((next((item["TaxId"] for item in entry["LineageEx"] if item["Rank"] == rank), None)))
+            # Checks if it is Protozoa TaxId
             elif ("5794" in (entry["TaxId"], (next((item["TaxId"] for item in entry["LineageEx"] if item["TaxId"] == "5794"), None)))
                     or "33682" in (entry["TaxId"], (next((item["TaxId"] for item in entry["LineageEx"] if item["TaxId"] == "33682"), None)))):
                 for rank in ranks:
@@ -185,11 +193,12 @@ def sort_tax_data(data_list, origin):
             else:
                 rest.add(entry["TaxId"])
     except:
-        print(f"falty is {trying}")
-    
+        print(f"falty is {entry}")
+    # Unifies the results in a single dictionary
     result_dict = {"bacteria": bacteria, "archaea": archaea, "viruses": viruses, "fungi": fungi, "protozoa": protozoa, "rest": rest}
     return result_dict
 
+# Function to save the result
 def save_file(file_name, result_dict, ranks):
     """Saves the data to a CSV file."""
     with open(file_name, "w+") as csv_out:
